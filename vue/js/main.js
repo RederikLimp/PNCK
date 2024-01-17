78f06716aa700787386a9193ca89f77df5af9454
RederikLimp <kirill.alisa.2004@mail.ru>
2024-01-17 09:39:01 +0700

    Задание 5

--- vue/js/main.js
+++ vue/js/main.js
@@ -11,14 +11,39 @@ let app = new Vue({
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
-            variantId: 2234,
-            variantColor: 'green'
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
             },
             {
-            variantId: 2235,
-            variantColor: 'blue'
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
-            ],
-            sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],            
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",}
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        cart: 0,
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        }
+    },
+    variants: [
+        {
+            variantId: 2234,
+            variantColor: 'green',
+            variantImage: "./assets/vmSocks-green-onWhite.jpg",
+        },
+        {
+            variantId: 2235,
+
+            variantColor: 'blue',
+            variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+        }
+    ],
 })
